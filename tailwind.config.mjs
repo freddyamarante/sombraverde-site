625d539d49259260d94eb931466b20c563423b88
freddy <freddyamarante14@gmail.com>
2024-01-31 17:55:12 -0400

    setup tailwind + react

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        orange: '#FA8006',
+        sun: '#FEE506',
+        ocean: '#0252D8',
+        palm: '#0F6C18',
+        night: {
+          light: '#234332',
+          DEFAULT: '#0E1B14',
+        },
+        pale: '#FBFDFC',
+      },
+    },
+  },
+  plugins: [],
 }
